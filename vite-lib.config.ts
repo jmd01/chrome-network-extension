3cd33f378136bce87f686bdc91cadc3866e990a1
John Davison <john01davison@gmail.com>
2021-06-21 19:45:09 +0100

    sorted build

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+    plugins: [reactRefresh()],
+    root: "dist/build",
+    build: {
+        rollupOptions: {
+            // https://rollupjs.org/guide/en/#big-list-of-options
+            // input: {
+            //     "devtools": "src/main.tsx",
+            //     "backgroundPage": "src/backgroundPage.ts"
+            // },
+            // output: {
+            //     entryFileNames: '[name].js'
+            // }
+            input: {
+                "main.js": "src/main.tsx",
+                "background.js": "src/backgroundPage.ts"
+            },
+            output: {
+                entryFileNames: '[name]'
+            }
+
+        },
+        // emptyOutDir: false
+    }
+
 })
